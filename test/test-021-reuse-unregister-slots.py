ac4f163dbd763b3a10a163a2117430b6470c8388
Julian Friedrich <julian.friedrich@gmx.at>
2025-04-22 22:19:44 +0200

    Added test-021-reuse-unregister-slots.py for slot reuse after UNREGISTER

---
+++ test/test-021-reuse-unregister-slots.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+#
+# test-021-reuse-unregister-slots.py – detect resource leaks in repeated UNREGISTER cycles
+#
+# This test registers and immediately unregisters clients in a loop,
+# repeating the cycle MAX_CLIENTS * 2 times to ensure that the broker
+# releases internal resources properly. If there is a resource leak or
+# improper slot cleanup, the broker will reject registration with ERROR.
+#
+# Copyright (C) 2025 Julian Friedrich
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+# This file is part of the wd-broker project:
+#   https://github.com/fjulian79/wd-broker
+#
+# Please feel free to open issues or contribute improvements.
+
+from common import *
+
+for i in range(MAX_CLIENTS * 2):
+    label = f"register/unregister cycle {i+1}"
+    clientID = register("leaktest", 10000)
+    check_cmd(label, f"UNREGISTER {clientID}\n", expect="OK")
